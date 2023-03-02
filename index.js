400ce6953cf2a575c9d7156f581197a838e4d3e1
pfbanco <pfbanco@gmail.com>
2023-03-01 23:56:57 -0300

    Segunda entrega

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-class ProductManager {
+/*class ProductManager {
   constructor() {
     this.products = [];
   }
@@ -67,3 +67,103 @@ console.log(productoNuevo1.getProductById(4));
 
 console.log(productoNuevo1.getProductById(3));
 //Traigo un elemento con getProductByID.
+*/
+
+const fs = require("fs");
+
+//const path = "Usuarios.json";
+
+class ManagerUsuarios {
+  constructor(path) {
+    this.path = path;
+  }
+
+  consultarUsuarios = async () => {
+    if (fs.existsSync(this.path)) {
+      const infoArchivo = await fs.promises.readFile(this.path, "utf-8");
+      const usuarios = JSON.parse(infoArchivo);
+      return usuarios;
+    } else {
+      console.log("Archivo no existe");
+      return [];
+    }
+  };
+
+  consultarUsuariosById = async (id) => {
+    const usuarios = await this.consultarUsuarios();
+    const user = usuarios.find((u) => u.id === id);
+    if (user) {
+      return user;
+    } else {
+      return "User not found";
+    }
+  };
+
+  crearUsuario = async (usuario) => {
+    const usuarios = await this.consultarUsuarios();
+    const id = this.#generarId(usuarios);
+    const nuevoUsuario = { id, ...usuario };
+    usuarios.push(nuevoUsuario);
+    await fs.promises.writeFile(this.path, JSON.stringify(usuarios));
+    return nuevoUsuario;
+  };
+
+  eliminarUsuarios = async () => {
+    if (fs.existsSync(this.path)) {
+      await fs.promises.unlink(this.path);
+      return 'Usuarios eliminados';
+    } else {
+      return 'No existe este archivo';
+    }
+  };
+
+  eliminarUsuariosById = async (id) => {
+    const usuarios = await this.consultarUsuarios();
+    const arrayNuevo = usuarios.filter(u=>u.id !== id)
+    await fs.promises.writeFile(this.path, JSON.stringify(arrayNuevo))
+  };
+
+  actualizarUsuario = async(id, obj) =>{
+    const usuarios = await this.consultarUsuarios();
+    const indexUsuario = usuarios.findIndex(u=>u.id === id)
+    if (indexUsuario === -1){
+      return 'Usuario no encontrado'
+    }
+    const usuarioActualizado = {...usuarios[indexUsuario], ...obj}
+    usuarios.splice(indexUsuario, 1,usuarioActualizado)
+    await fs.promises.writeFile(this.path, JSON.stringify(usuarios))
+  }
+
+  #generarId = (usuarios) => {
+    let id;
+    if (usuarios.length === 0) {
+      id = 1;
+    } else {
+      id = usuarios[usuarios.length - 1].id + 1;
+    }
+    return id;
+  };
+}
+
+const usuario1 = {
+  nombre: "Fran",
+  apellido: "Banco",
+};
+
+const usuario2 = {
+  nombre: "Pepito",
+  apellido: "Gomez",
+};
+
+async function prueba() {
+  const manager = new ManagerUsuarios("Usuarios.json");
+  await manager.crearUsuario(usuario2);
+  await manager.crearUsuario(usuario1);
+  const usuarios = await manager.consultarUsuarios();
+  //const usuario = await manager.consultarUsuariosById(15)
+  //await manager.eliminarUsuariosById(10)
+  //await manager.eliminarUsuarios()
+  console.log(usuarios);
+}
+
+prueba();
